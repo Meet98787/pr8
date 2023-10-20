7842adafcaea16ea18696b3aa24666f253407b75
meet98787 <meetlakhani98787@gmail.com>
2023-10-20 10:42:35 +0530

    pr8-9

--- src/component/Rutering/Home.jsx
+++ src/component/Rutering/Home.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <h1 className='text-center'>Walcome to Student Manegment System</h1>
+    <h1 className='text-center'>Welcome to Student Managment System</h1>
   )
 }
